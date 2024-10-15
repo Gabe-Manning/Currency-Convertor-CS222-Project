58f39956eb52846f369dd48e98893f7b2f8c7a86
joseph-sobiech <joseph.sobiech@bsu.edu>
2024-10-15 18:28:09 -0400

    Added .json file, API Connector test

--- src/test/java/edu/bsu/cs222/test/APIConnectorTest.java
+++ src/test/java/edu/bsu/cs222/test/APIConnectorTest.java
@@ -1,9 +1,18 @@
 package edu.bsu.cs222.test;
 
+import edu.bsu.cs222.main.APIConnector;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class APIConnectorTest {
 
     @Test
-    public void
+    public void getConnectedTest() {
+        APIConnector connector = new APIConnector();
+        HttpsURLConnection connection = connector.getConnected();
+        Assertions.assertNotNull(connection);
+    }
+
 }
