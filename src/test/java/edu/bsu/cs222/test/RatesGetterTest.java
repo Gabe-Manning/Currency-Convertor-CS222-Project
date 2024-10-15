fe2d6ffbf0f9c3d4df6ec7c65d1d8b363c94420a
joseph-sobiech <joseph.sobiech@bsu.edu>
2024-10-15 18:48:19 -0400

    Added to RatesGetterTest

--- src/test/java/edu/bsu/cs222/test/RatesGetterTest.java
+++ src/test/java/edu/bsu/cs222/test/RatesGetterTest.java
@@ -1,4 +1,21 @@
 package edu.bsu.cs222.test;
 
+import edu.bsu.cs222.main.APIConnector;
+import edu.bsu.cs222.main.RatesGetter;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import javax.net.ssl.HttpsURLConnection;
+import java.io.IOException;
+
 public class RatesGetterTest {
+    @Test
+    public void getRatesTest() throws IOException {
+        RatesGetter testRates = new RatesGetter();
+        APIConnector connector = new APIConnector();
+        HttpsURLConnection connection = connector.getConnected();
+        String testResult = testRates.getRates(connection);
+        String testAgainst = "currencies.json";
+        Assertions.assertEquals(testResult, testAgainst);
+    }
 }
