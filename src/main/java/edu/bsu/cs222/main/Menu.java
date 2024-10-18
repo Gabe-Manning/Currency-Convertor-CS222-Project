ccaa9852dba4255c66af5939cfe3d431728b3e88
Hosman2 <152414994+Hosman2@users.noreply.github.com>
2024-10-18 18:05:27 -0400

    Updated Menu

--- src/main/java/edu/bsu/cs222/main/Menu.java
+++ src/main/java/edu/bsu/cs222/main/Menu.java
@@ -6,27 +6,43 @@ public class Menu {
     private Scanner scanner = new Scanner(System.in);
     public void displayMenu() {
         while (true) {
-            String selection;
+            String menuSelection;
             System.out.printf("\n%s MENU %s\n", "*".repeat(9), "*".repeat(9));
             System.out.println("Please make a selection:\n" +
                     "1) Convert Currency(with or without amount)\n" +
-                    "2) Get Historical Record\n"
+                    "2) Get Historical Record\n" +
+                    "3) View All Exchange Rates\n"
             );
-            selection = scanner.nextLine();
+            menuSelection = scanner.nextLine();
 
-            if (selection.equals("1")){
+            if (menuSelection.equals("1")){
                 convertCurrency();
-            } else if (selection.equals("2")) {
+            } else if (menuSelection.equals("2")) {
                 historyData();
+            } else if (menuSelection.equals("3")) {
+
             }
+
         }
     }
 
     public void convertCurrency(){
+        String convertSelection = scanner.nextLine();
+        String startingCurrency, finalCurrency;
+        System.out.println("Enter starting currency: ");
+        startingCurrency = scanner.nextLine();
+        System.out.println("Enter final currency: ");
+        finalCurrency = scanner.nextLine();
         System.out.println("Please make a selection:\n" +
-                "1) Convert With A Starting Amount\n" +
-                "2) View Exchange Rates");
+                "1) Convert Currencies With A Starting Amount\n" +
+                "2) View Specific Exchange Rates\n");
 
+        if(convertSelection.equals("1")){
+            String startingAmount;
+            System.out.println("Enter Starting Amount: ");
+            startingAmount = scanner.nextLine();
+
+        }
     }
 
     public void historyData(){
