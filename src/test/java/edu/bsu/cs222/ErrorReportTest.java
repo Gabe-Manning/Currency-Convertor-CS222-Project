774af14b0aa46aeb3a4c6643c4631ff1091568df
joseph-sobiech <joseph.sobiech@bsu.edu>
2024-10-24 16:59:39 -0400

    Finished Error Report Test

--- src/test/java/edu/bsu/cs222/ErrorReportTest.java
+++ src/test/java/edu/bsu/cs222/ErrorReportTest.java
@@ -8,30 +8,61 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 public class ErrorReportTest {
-
-
-/*@Test
-public void checkEmptyInputConsoleTest() {
-    String input = "Not empty";
-    ErrorReport errorChecker = new ErrorReport();
-    errorChecker.checkEmptyInput(input);
-    Assertions.assertNull(input);
-} */
+    ErrorReport errorReport = new ErrorReport();
 
     @Test
-    public void checkConnectionStatusConsoleTest() throws MalformedURLException {
-        String urlString = "https://api.exchangeratesapi.io/v1/latest?access_key=20d06127f90b41956b66466007af69d1&format=1";
+    public void checkConnectionStatusTest() throws MalformedURLException {
+        APIConnector connector = new APIConnector();
+        String apiKey = connector.getAPIKey();
+        String urlString = "https://api.exchangeratesapi.io/v1/latest?access_key=" + apiKey + "&format=1";
         URL urlActual = new URL(urlString);
         ErrorReport errorChecker = new ErrorReport();
         errorChecker.checkConnectionStatus(urlActual);
         Assertions.assertNotNull(urlActual);
     }
-
     @Test
-    public void checkValidInputTest() throws IOException {
-        RatesParser ratesParser = new RatesParser();
-        String testInputCurrency = "USD";
-        String testOutputCurrency = "CAD";
-        //ratesParser.parse
+    public void checkEmptyInputTest() {
+        String input = "";
+        boolean result = errorReport.checkEmptyInput(input);
+        Assertions.assertTrue(result);
     }
+    @Test
+    public void checkSupportedCurrencyTest() throws IOException {
+        String currency = "USD";
+        boolean result = errorReport.checkSupportedCurrency(currency);
+        Assertions.assertFalse(result);
+
+    }
+    @Test
+    public void checkForUnparseableCharactersTest() {
+        String input = "*";
+        boolean result = errorReport.checkForUnparseableCharacters(input);
+        Assertions.assertTrue(result);
+    }
+    @Test
+    public void checkInputAmountCanBeFloatTest() {
+        String input = "12";
+        boolean result = errorReport.checkInputAmountCanBeFloat(input);
+        Assertions.assertFalse(result);
+    }
+    @Test
+    public void checkDateInputIsCorrectFormatTest() {
+        String input = "2024-01-01";
+        boolean result = errorReport.checkDateInputIsCorrectFormat(input);
+        Assertions.assertTrue(result);
+    }
+    @Test
+    public void checkDateIsValidForAPITest() {
+        String input = "2024-01-01";
+        boolean result = errorReport.checkDateIsValidForAPI(input);
+        Assertions.assertFalse(result);
+    }
+    @Test
+    public void doesValidDateContainDataTest() throws IOException {
+        String inputDate = "2024-01-01";
+        String inputCurrency = "USD";
+        boolean result = errorReport.doesValidDateContainData(inputCurrency, inputDate);
+        Assertions.assertFalse(result);
+    }
+
 }
