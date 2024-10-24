ac188b29435bc68a66da61e63fa0afb06e5430c4
joseph-sobiech <joseph.sobiech@bsu.edu>
2024-10-24 15:46:05 -0400

    Added Converter Test

---
+++ src/test/java/edu/bsu/cs222/ConverterTest.java
@@ -0,0 +1,35 @@
+package edu.bsu.cs222;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ConverterTest {
+    Converter converter = new Converter();
+    @Test
+    public void testConvertUsingOnlyCurrencies() {
+        List<Float> testList = new ArrayList<>();
+        testList.add(2.5F);
+        testList.add(5F);
+        float rate = converter.convertUsingOnlyCurrencies(testList);
+        Assertions.assertEquals(rate, 2F);
+    }
+    @Test
+    public void testConvertUsingCurrenciesAndAmount() {
+        List<Float> testList = new ArrayList<>();
+        testList.add(2.5F);
+        testList.add(5F);
+        float testRate = converter.convertUsingOnlyCurrencies(testList);
+        float testStartingAmount = 10F;
+        float endingAmount = testRate * testStartingAmount;
+        Assertions.assertEquals(endingAmount, 20F);
+    }
+    @Test
+    public void testRatesGoneUpOrDown() {
+        float difference = 0.3F;
+        String upOrDown = converter.ratesGoneUpOrDown(difference);
+        Assertions.assertEquals(upOrDown, "increases");
+    }
+}
