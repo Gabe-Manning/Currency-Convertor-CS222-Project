606f7582bbea4762fba4e4e4c4852d994721bcf3
Christian Johnson <5419cjohnson@gmail.com>
2024-10-29 10:25:23 -0400

    Adds SortingTest

--- src/test/java/edu/bsu/cs222/SortingTest.java
+++ src/test/java/edu/bsu/cs222/SortingTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 
 public class SortingTest {
     public String testMethod() {
-        return null;
+        String hello = "hello";
+        return hello;
     }
 }
