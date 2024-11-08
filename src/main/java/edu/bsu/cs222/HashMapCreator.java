d65dca1e5b46308e61f7c86de6067e558acf02d7
joseph-sobiech <joseph.sobiech@bsu.edu>
2024-11-08 18:36:17 -0500

    Added createAllRatesForHashMap

--- src/main/java/edu/bsu/cs222/HashMapCreator.java
+++ src/main/java/edu/bsu/cs222/HashMapCreator.java
@@ -1,23 +1,28 @@
 package edu.bsu.cs222;
 
+import com.jayway.jsonpath.JsonPath;
+import net.minidev.json.JSONArray;
+import net.minidev.json.JSONObject;
+
 import javax.net.ssl.HttpsURLConnection;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Scanner;
 
 public class HashMapCreator {
     RatesGetter ratesGetter = new RatesGetter();
     APIConnector APIConnector = new APIConnector();
 
-    public HashMap<String, Float> createHashMap(String jsonValue) throws IOException {
+    public HashMap<String, Float> createHashMap() throws IOException {
 
         ArrayList<CurrencyBuilder> dataList = new ArrayList<>();
         HashMap<String, Float> hashMap = new HashMap<>();
         HttpsURLConnection API_connection = APIConnector.connectNoDate();
-        String allRatesOnSpecificDate = ratesGetter.getRates(API_connection);
+        String allRates = createAllRatesForHashMap();
 
-        try(Scanner scanner = new Scanner(allRatesOnSpecificDate)) {
+        try(Scanner scanner = new Scanner(allRates)) {
             ArrayList<String> lines = new ArrayList<>();
 
             while(scanner.hasNextLine()) {
@@ -26,6 +31,7 @@ public class HashMapCreator {
 
             for (int i = 0; i < lines.size(); i++) {
                 String line = lines.get(i);
+                line = line.replace(",", "");
                 String [] data = line.split(":");
                 float valueID = Float.parseFloat(data[1]);
                 String currencyAbb = data[0];
@@ -34,9 +40,21 @@ public class HashMapCreator {
                 //instantiation
                 CurrencyBuilder currencyBuilder = new CurrencyBuilder(currencyAbb, valueID);
                 dataList.add(currencyBuilder);
-                //hashMap.put(dataList.get(i));
+                hashMap.put(currencyAbb, valueID);
             }
         }
-        return null;
+        System.out.println(hashMap);
+        return hashMap;
+    }
+    public String createAllRatesForHashMap() throws IOException {
+        StringBuilder hashMapToString = new StringBuilder();
+
+        HttpsURLConnection API_connection = APIConnector.connectNoDate();
+        String allCurrentRates = ratesGetter.getRates(API_connection);
+
+
+        JSONArray abbreviationsAndRates = JsonPath.read(allCurrentRates, "$..rates" );
+
+        return abbreviationsAndRates.toJSONString();
     }
 }
