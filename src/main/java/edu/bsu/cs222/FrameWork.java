8851f01afa19d4406b0fc1b0d3d41f37b4346c71
joseph-sobiech <joseph.sobiech@bsu.edu>
2024-11-19 21:01:58 -0500

    Updates FrameWork and Interface to more cleanly call and implement ActionListener, Still test code

--- src/main/java/edu/bsu/cs222/FrameWork.java
+++ src/main/java/edu/bsu/cs222/FrameWork.java
@@ -1,24 +1,36 @@
 package edu.bsu.cs222;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class FrameWork extends JFrame{
-    void myFrame() {
-        ImageIcon image = new ImageIcon("IMG_1702.jpeg");
-
+public class FrameWork extends JFrame implements ActionListener {
+    JButton button;
+     FrameWork() {
+        ImageIcon image = new ImageIcon("IMG_1702");
         JLabel label = new JLabel();
         label.setIcon(image);
 
-        JButton button = new JButton();
-        button.setBounds(20, 30, 10, 20);
-        button.setText("Test");
+
+        button = new JButton();
+        button.setBounds(100, 50, 150, 150);
+        button.setText("test");
+        button.addActionListener(this);
+
 
         this.setTitle("Currency Converter");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setSize(500, 500);
+        this.setLayout(null);
+        this.setSize(700, 500);
         this.setVisible(true);
         this.add(label);
         this.add(button);
+    }
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == button) {
+            System.out.println("Test");
+        }
     }
 }
--- src/main/java/edu/bsu/cs222/InterfaceMain.java
+++ src/main/java/edu/bsu/cs222/InterfaceMain.java
@@ -3,8 +3,7 @@ package edu.bsu.cs222;
 
 public class InterfaceMain {
     public static void main(String[] args) {
-        FrameWork frameWork = new FrameWork();
-        frameWork.myFrame();
+        new FrameWork();
 
     }
 }
