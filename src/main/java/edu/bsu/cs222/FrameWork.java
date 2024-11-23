9154f2bd9c6e2598327686031a8ed6515bbe311c
Hosman2 <152414994+Hosman2@users.noreply.github.com>
2024-11-23 17:55:40 -0500

    Adds more button and layout to FrameWork GUI.

--- src/main/java/edu/bsu/cs222/FrameWork.java
+++ src/main/java/edu/bsu/cs222/FrameWork.java
@@ -1,35 +1,75 @@
 package edu.bsu.cs222;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class FrameWork extends JFrame implements ActionListener {
-    JButton button;
+    JButton GetRatesButton;
+    JButton convertRatesButton;
+    JButton historyButton;
+    JButton strongestAndWeakestButton;
+    JButton globalRankingButton;
+
      FrameWork() {
         ImageIcon image = new ImageIcon("IMG_1702");
         JLabel label = new JLabel();
         label.setIcon(image);
 
+        JFrame applicationFrame = new JFrame();
 
-        button = new JButton();
-        button.setBounds(100, 50, 150, 150);
-        button.setText("test");
-        button.addActionListener(this);
+        JPanel sidePanel = new JPanel();
+        JPanel topPanel = new JPanel();
+        sidePanel.setBackground(Color.GREEN);
+        topPanel.setBackground(Color.GREEN);
+        sidePanel.setPreferredSize(new Dimension(200, 400));
+        topPanel.setPreferredSize(new Dimension(400,200));
 
+        GetRatesButton = new JButton();
+        GetRatesButton.setBounds(25, 25, 150, 150);
+        GetRatesButton.setText("Get Rates");
+        GetRatesButton.addActionListener(this);
 
-        this.setTitle("Currency Converter");
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setLayout(null);
-        this.setSize(700, 500);
-        this.setVisible(true);
-        this.add(label);
-        this.add(button);
+        convertRatesButton = new JButton();
+        convertRatesButton.setBounds(25, 200, 150, 150);
+        convertRatesButton.setText("Convert Currency");
+        convertRatesButton.addActionListener(this);
+
+        historyButton = new JButton();
+        historyButton.setBounds(25, 375, 150, 150);
+        historyButton.setText("History");
+        historyButton.addActionListener(this);
+
+        strongestAndWeakestButton = new JButton();
+        strongestAndWeakestButton.setBounds(25, 550, 150, 150);
+        strongestAndWeakestButton.setText("Strongest/Weakest");
+        strongestAndWeakestButton.addActionListener(this);
+
+        globalRankingButton = new JButton();
+        globalRankingButton.setBounds(25, 725, 150, 150);
+        globalRankingButton.setText("Global Ranking");
+        globalRankingButton.addActionListener(this);
+
+        applicationFrame.setTitle("Currency Converter");
+        applicationFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        applicationFrame.setSize(1000, 800);
+        applicationFrame.setVisible(true);
+        applicationFrame.add(label);
+        applicationFrame.add(GetRatesButton);
+        applicationFrame.add(convertRatesButton);
+        applicationFrame.add(historyButton);
+        applicationFrame.add(strongestAndWeakestButton);
+        applicationFrame.add(globalRankingButton);
+        applicationFrame.add(sidePanel, BorderLayout.WEST);
+        applicationFrame.add(topPanel, BorderLayout.NORTH);
+
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == button) {
+        if (e.getSource() == GetRatesButton) {
             System.out.println("Test");
         }
     }
