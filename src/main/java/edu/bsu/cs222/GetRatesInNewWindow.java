764177cb2421eaf5723fe8db1fbf5faa6df34d9a
joseph-sobiech <joseph.sobiech@bsu.edu>
2024-11-25 18:01:07 -0500

    Adds actions to buttons and getRatesButton opens new window

--- src/main/java/edu/bsu/cs222/GetRatesInNewWindow.java
+++ src/main/java/edu/bsu/cs222/GetRatesInNewWindow.java
@@ -2,13 +2,16 @@ package edu.bsu.cs222;
 
 import javax.swing.*;
 import java.awt.*;
+import java.io.IOException;
 
 public class GetRatesInNewWindow {
+    APICallForRates APICallForRates = new APICallForRates();
+    String allRates = APICallForRates.getStringDataNoDate();
     JFrame frame = new JFrame();
-    JLabel label = new JLabel("Rates: ");
-    GetRatesInNewWindow() {
-        label.setBounds(0, 0, 100, 50);
-        label.setFont(new Font(null, Font.PLAIN, 25));
+    JLabel label = new JLabel(allRates);
+    GetRatesInNewWindow() throws IOException {
+        label.setBounds(0, 0, 50, 100);
+        label.setFont(new Font(null, Font.PLAIN, 10));
         frame.add(label);
 
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
